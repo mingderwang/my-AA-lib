8ace5b3424c79725b3e297bd480aec4be501a11b
mingderwang <admin@ming-ders-MacBook.local>
2024-02-06 23:29:06 +0800

    init

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,24 @@
-export const one = 1
-export const two = 2
+import { parseUnits, Wallet, getDefaultProvider, JsonRpcProvider } from 'ethers';
+
+
+async function signTransaction() {
+
+  // Create a Wallet
+  const privateKey = '0xac0974bec39a17e36ba4a6b4d238ff944bacb478cbed5efcae784d7bf4f2ff80';
+  const url = 'http://127.0.0.1:8545'
+  const provider = new JsonRpcProvider(url);
+  const signer = new Wallet(privateKey, provider);
+
+  // Recipient address and amount to send (in Wei)
+  const toAddress = '0x70997970C51812dc3A010C7d01b50e0d17dc79C8';
+
+  const tx = await signer.sendTransaction({
+        to: toAddress,
+        value: parseUnits('0.666', 'ether'),
+      });
+      console.log(tx);
+}
+
+// Call the function to sign the transaction
+signTransaction();
+
